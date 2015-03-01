f45652b7e338b64e9944f5e550c85487a30e5c6e
Carles F. Julià <carles@protopixel.net>
2015-03-01 13:17:07 +0100

    copying openframeworks.py when regenerating bindings

--- generatebindings.sh
+++ generatebindings.sh
@@ -2,3 +2,7 @@
 
 cd swig
 make desktop LANG=python SWIG=swig3.0 DEST_LANG_DIR=../lib
+cd ..
+echo copying openframeworks.py to examples
+cp lib/openframeworks.py example_ScriptTester/bin/data/
+cp lib/openframeworks.py example_simpleScript/bin/data/
