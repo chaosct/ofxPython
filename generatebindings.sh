e27d540ea0ab1263fffab8f23f4d373c742e00bb
Carles F. Julià <carles@protopixel.net>
2015-10-13 14:07:11 +0200

    using swig instead of swig3.0 when generating bindings

--- generatebindings.sh
+++ generatebindings.sh
@@ -1,11 +1,12 @@
 #/bin/sh
 
 cd swig-of
-make desktop LANG=python SWIG=swig3.0 DEST_LANG_DIR=../lib
+make desktop LANG=python DEST_LANG_DIR=../lib
 cd ..
-cd swig-addicional
+cd swig-additional
 make
 cd ..
+
 echo copying openframeworks.py to examples
 cp lib/* example_ScriptTester/bin/data/
 cp lib/* example_simpleScript/bin/data/
